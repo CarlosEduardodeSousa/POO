4cd45568a756cca3b2e30f4ba065364198e741cc
CarlosEduardodeSousa <carloseduardosousa378@gmail.com>
2025-03-16 13:03:16 -0300

    Aula da faculdade

---
+++ Aula03/ContaCorrente.java
@@ -0,0 +1,69 @@
+package Aula03;
+
+public class ContaCorrente {
+    
+    public String nome;
+    public float saldo;
+    public float limite;
+    public char tipo;
+
+    public ContaCorrente(String nome, float saldo, float limite, char tipo){
+        this.nome = nome;
+        this.saldo = saldo;
+        this.limite = limite;
+        this.tipo = tipo;
+    }
+
+    public ContaCorrente(String nome, float saldo, char tipo){
+        this.nome = nome;
+        this.saldo = saldo;
+        this.tipo = tipo;
+    }
+
+    public ContaCorrente(){
+
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public float getSaldo(){
+        return saldo;
+    }
+
+    public void setsaldo(float saldo){
+        this.saldo = saldo;
+    }
+
+    public float getLimite(){
+        return limite;
+    }
+
+    public void setLimite( float limite){
+        this.limite = limite;
+    }
+
+    public char getTipo(){
+        return tipo;
+    }
+
+    public void setTipo(char tipo){
+        this.tipo = tipo;
+    }
+
+    public float depositar(float valor){
+        float saldo = 0;
+        saldo = saldo + valor;
+        return  saldo;
+    }
+
+    public float atualizarSaldo(float saldo, float valor){
+        float novo_saldo = saldo + valor;
+        return novo_saldo;
+    }
+          
+}
---
+++ Aula03/ProjetoContaCorrente.java
@@ -0,0 +1,35 @@
+package Aula03;
+
+import javax.swing.JOptionPane;
+
+public class ProjetoContaCorrente {
+    public static void main(String[] args) {
+
+        ContaCorrente conta, conta1;
+
+        String n = JOptionPane.showInputDialog("Digite o nome do cliente: ");
+
+        float s = Float.parseFloat(JOptionPane.showInputDialog("Digite o saldo da conta:"));
+        float l = Float.parseFloat(JOptionPane.showInputDialog("Digite o limite da conta:"));
+        char t = JOptionPane.showInputDialog("Digite o tipo da conta (N ou E):").charAt(0);
+        conta = new ContaCorrente(n,s,l,t);
+
+
+        JOptionPane.showMessageDialog(null, "Nome: " + conta.nome + " \nTipo da conta: " + conta.tipo
+        + " \nSaldo: " + conta.saldo + " \nLimite: " + conta.limite);
+
+        n = JOptionPane.showInputDialog("Digite o nome do cliente:");
+        s = Float.parseFloat(JOptionPane.showInputDialog("Digite o saldo da conta:"));
+        t = JOptionPane.showInputDialog("Digite o tipo da conta (N ou E):").charAt(0);
+        conta1 = new ContaCorrente(n, s, t);
+
+        JOptionPane.showMessageDialog(null, "Nome: " + conta1.nome + " \nTipo da conta: " + conta1.tipo
+                + " \nSaldo: " + conta1.saldo);
+
+        float valor = Float.parseFloat(JOptionPane.showInputDialog("Digite o valor do deposito: "));
+        float deposito = conta.depositar(valor);
+        JOptionPane.showMessageDialog(null, "Valor depositado: " + deposito);
+        float saldo_atual = conta.atualizarSaldo(s, valor);
+        JOptionPane.showMessageDialog(null, "Saldo após o deposito: " + saldo_atual);
+    }
+}
