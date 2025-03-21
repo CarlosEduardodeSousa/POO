04857955fdb8e644610a9f8c246d04e39bba29d6
CarlosEduardodeSousa <carloseduardosousa378@gmail.com>
2025-03-21 16:05:00 -0300

    Aula 05

---
+++ Aula05/TesteConta.java
@@ -0,0 +1,16 @@
+package Aula05;
+
+import javax.swing.JOptionPane;
+
+public class TesteConta {
+    public static void main(String[] args) {
+        Conta c = new Conta("1134", 1000.50);
+        Conta c1 = new Conta("1222", 2050.45);
+
+        c.imprimeDados();
+        c.sacarValor(500.50);
+        c.imprimeDados();
+        c.sacarValor(700);
+        JOptionPane.showMessageDialog(null, "Maior saldo: " + c.maiorSaldo(c1));
+    }
+}
